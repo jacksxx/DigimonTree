894f1956a89ba123de4e95d89db91b23afec064a
L <100241773+jacksxx@users.noreply.github.com>
2024-03-26 15:42:55 -0300

    feat: update DigimonService to include caching mechanism

--- digimon-tree/src/services/DigimonService.ts
+++ digimon-tree/src/services/DigimonService.ts
@@ -1,20 +1,24 @@
+import { Digimon } from "@/types/Digimon";
 import axios from "axios";
 
-export const getAllDigimons = async () => {
+const API_BASE = "https://digi-api.com/api/v1/digimon";
+
+export const getAllDigimons = async (): Promise<Digimon[]> => {
   try {
-    const res = await axios.get("digi-api.com/api/v1/digimon");
-    console.log(res)
-    return res.data;
+    const res = await axios.get(API_BASE);
+    const data = res.data;
+    const digimons: Digimon[] = data.content;
+    return digimons;
   } catch (error) {
-    throw new Error(`Erro ao buscar produtos`);
+    throw new Error(`Erro ao buscar digimons: ${error}`);
   }
 };
 
-export const getDigimonByName = async (name: string) => {
+export const getDigimonById = async (id: number): Promise<Digimon> => {
   try {
-    const res = await axios.get(`digi-api.com/api/v1/digimon/${name}`);
+    const res = await axios.get(`${API_BASE}/${id}`);
     return res.data;
   } catch (error) {
-    throw new Error(`Erro ao buscar produtos`);
+    throw new Error(`Erro ao buscar o digimon ${id}: ${error}`);
   }
 };
