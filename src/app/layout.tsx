4e039bbe001b04e67e65642afd289429de73fdbd
L. <100241773+jacksxx@users.noreply.github.com>
2024-05-15 17:39:18 -0300

    Revert "feat: layout update"
    This reverts commit 3d4c1b1e7f675421c4c25beb7db6b81626f4144c.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import MComp from "@/components/MainComponent/MComp";
-import { QueryClientProvider } from "@tanstack/react-query";
-import Navbar from "@/components/NavBar/Navbar";
-import Footer from "@/components/Footer/Footer";
-import { queryClient } from "@/libs/queryClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Digimon Tree",
-  description: "Search for your favorite digimon here!",
+  description: "Search for your favorite digimon here!",  
 };
 
 export default function RootLayout({
@@ -20,13 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br">      
       <body className={inter.className}>
-        <Navbar />
-        <QueryClientProvider client={queryClient}>
-          <div className="px-10 mb-10 pb-10 pt-2">{children}</div>
-        </QueryClientProvider>
-        <Footer />
+        <MComp>{children}</MComp>
       </body>
     </html>
   );
