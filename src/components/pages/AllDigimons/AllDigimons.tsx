bbf7faf4a374e330c30542f7f040a629c3dd3440
L. <100241773+jacksxx@users.noreply.github.com>
2025-03-21 09:36:08 -0300

    update packages

--- src/components/pages/AllDigimons/AllDigimons.tsx
+++ src/components/pages/AllDigimons/AllDigimons.tsx
@@ -4,53 +4,25 @@ import DigimonList from "@/components/pages/AllDigimons/DigimonList";
 import Loading from "@/components/common/Loading/Loading";
 import NoDataMessage from "@/components/common/NoData/NoDataMessage";
 import Pagination from "@/components/Pagination/Pagination";
-import { useState } from "react";
 import * as S from "./styles";
-import { useGetAllLevels } from "@/services/levels/queires";
-import { useGetAllAttributes } from "@/services/attibutes/queries";
-import { useGetAllDigimons } from "@/services/digimons/queries";
 import Filters from "./Filters/Filters";
+import { UseAllDigimons } from "@/hooks/useAllDigimons";
 
 const AllDigimons = () => {
-  const [pagination, SetPagination] = useState<{
-    page: number;
-    pageSize: number;
-  }>({ page: 0, pageSize: 20 });
-  const [digimonName, setDigimonName] = useState<string>("");
-  const [filters, setFilters] = useState<{
-    attribute: string;
-    level: string;
-    xAntibody: boolean;
-  }>({ attribute: "", level: "", xAntibody: false });
-
-  const { digimons, isError, isLoading, pageable } = useGetAllDigimons({
-    page: pagination.page,
-    pageSize: pagination.pageSize,
-    name: digimonName,
-    attribute: filters.attribute,
-    level: filters.level,
-    xAntibody: filters.xAntibody,
-  });
-  const { levels } = useGetAllLevels();
-  const { attributes } = useGetAllAttributes();
-
-  const filterOptions: {
-    key: keyof Omit<typeof filters, "xAntibody">;
-    title: string;
-    options: { label: string; value: string }[];
-  }[] = [
-    {
-      key: "attribute",
-      title: "Atributo",
-      options: attributes.map((e) => ({ label: e.name, value: e.name })),
-    },
-    {
-      key: "level",
-      title: "Level",
-      options: levels.map((e) => ({ label: e.name, value: e.name })),
-    },
-  ];
-
+  const {
+    SetPagination,
+    digimonName,
+    digimons,
+    filterOptions,
+    filters,
+    isError,
+    isLoading,
+    pageable,
+    pagination,
+    setDigimonName,
+    setFilters,
+  } = UseAllDigimons();
+  
   if (isLoading) {
     return <Loading message="Carregando digimons" />;
   }
